d79b7ab99a1daeafbff757d5476fdad157d72ed4
Sam Boopathy <samuelfernandezcctrack@gmail.com>
2022-02-04 06:53:55 -0500

    added image file

--- src/app/component/dashboard/dashboard.component.ts
+++ src/app/component/dashboard/dashboard.component.ts
@@ -8,20 +8,44 @@ import { Component, OnInit } from '@angular/core';
 export class DashboardComponent implements OnInit {
   public users: Array<any>;
   public userSarvana: any;
-  public title: string; 
+  public title: string;
 
   constructor() {
     this.title = "User Detail";
-    this.users = [];
+    this.users = [
+      {
+        "name": "Siva",
+        "age": 35,
+        "gender": "male"
+      },
+      {
+        "name": "Nithyan",
+        "age": 60,
+        "gender": "male"
+      }
+    ];
     this.userSarvana = {
       "name": "Sarvana",
       "age": 30,
       "gender": "male"
     }
+    this.userDisplay();
   }
 
   ngOnInit(): void {
     // this.users = []
   }
+
+  public userDisplay() {
+    console.log("Finish all pending tasks");
+    console.log("Length of users object  ", this.users.length);
+    for (let i=0; i< this.users.length; i++){
+      console.log("User Number ",i);
+    }
+
+  }
+
+
+
 
 }
