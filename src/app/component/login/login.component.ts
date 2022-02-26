e1488d9b6978228791f539593371791c9dbf02d7
Sam Boopathy <samuelfernandezcctrack@gmail.com>
2022-02-26 11:32:10 -0500

    forEach While added

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -10,6 +10,7 @@ export class LoginComponent implements OnInit {
   public userdata;
   public submitButtonState: boolean;
   public screens: Array<string>;
+  public totalPrasad: number = 10;
   constructor() {    
     this.userdata = {
       "loginid": 'sample',
@@ -27,7 +28,29 @@ export class LoginComponent implements OnInit {
 
     console.log("Submit Form Called");
     console.log(this.userdata);
+    // this.screens.forEach(
+    //   (screen: string, i: number) => {
+    //     console.log(" Line 32 ",screen, i);
+
+    //   }
+    // )
+    
+    // for(let i=0; i<this.screens.length; i++) {
+    //   let screen = this.screens[i];
+    //   if (i != 1) {
+    //     console.log(" Line 40 ",screen, i);
+    //   }     
+    // } 
+    let isPrasadOver: boolean = false;
+    while(!isPrasadOver) {
+      console.log("take prasad", this.totalPrasad);
+      if (this.totalPrasad == 0) {
+        isPrasadOver = true;
+      }
+      this.totalPrasad = this.totalPrasad - 1;
+    }
 
   }
 
 }
+
